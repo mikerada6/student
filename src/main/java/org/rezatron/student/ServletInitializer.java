62686daa02217ce13b1f6a397660b81eba462588
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-14 23:39:10 -0400

    add import org.rezatron.student.StudentApplication;

--- src/main/java/org/rezatron/student/ServletInitializer.java
+++ src/main/java/org/rezatron/student/ServletInitializer.java
@@ -2,6 +2,7 @@ package org.rezatron.student;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.rezatron.student.StudentApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
